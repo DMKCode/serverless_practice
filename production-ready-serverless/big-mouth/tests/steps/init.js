6a7340ce6f2f9f2561b672bc2a060932c2cc8c56
David Kananda <davidkananda@gmail.com>
2019-04-29 06:32:35 +0100

    update init step

--- production-ready-serverless/big-mouth/tests/steps/init.js
+++ production-ready-serverless/big-mouth/tests/steps/init.js
@@ -14,8 +14,9 @@ let init = async () => {
     "https://udj82h717g.execute-api.us-east-1.amazonaws.com/dev/restaurants";
   process.env.restaurants_table = "restaurants";
   process.env.AWS_REGION = "us-east-1";
-  process.env.cognito_user_pool_id = "test_cognito_client_id";
   process.env.cognito_client_id = "test_cognito_user_pool_id";
+  process.env.cognito_user_pool_id = "us-east-1_DoRcnwa16";
+  process.env.cognito_server_client_id = "51qf54ln3hh63vct86dln3v8ge";
 
   let cred = await awscred.load((err, data) => {
     const {
