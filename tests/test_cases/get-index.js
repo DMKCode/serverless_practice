f9c9b65ddc40e8995520fc16dbccdbac456a8ef1
David Kananda <davidkananda@gmail.com>
2019-05-24 16:57:59 +0100

    update to course

--- tests/test_cases/get-index.js
+++ tests/test_cases/get-index.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict';
 
-const cheerio = require("cheerio");
-const expect = require("chai").expect;
-const when = require("../steps/when");
-const init = require("../steps/init").init;
+const cheerio = require('cheerio');
+const expect = require('chai').expect;
+const when = require('../steps/when');
+const init = require('../steps/init').init;
 
-describe("When we invoke the GET / endpoint", async () => {
+describe('When we invoke the GET / endpoint', async () => {
   beforeEach(async () => {
     await init();
   });
 
-  it("should return the index page with 8 restaurants", async () => {
+  it('should return the index page with 8 restaurants', async () => {
     let res = await when.we_invoke_get_index();
 
     expect(res.statusCode).to.equal(200);
-    expect(res.headers["content-type"]).to.equal("text/html; charset=UTF-8");
+    expect(res.headers['content-type']).to.equal('text/html; charset=UTF-8');
     expect(res.body).to.not.be.null;
 
     let $ = cheerio.load(res.body);
-    let restaurants = $(".restaurant", "#restaurantsUl");
+    let restaurants = $('.restaurant', '#restaurantsUl');
     expect(restaurants.length).to.equal(8);
   });
 });
